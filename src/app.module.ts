16bbb8f2826c057ef8792f25eb692934460a76ee
Lau1907 <130590443+Lau1907@users.noreply.github.com>
2026-03-09 19:48:00 -0600

    fix: Correcion del CRUD de usuarios y creacion de rutas para la autentificacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './common/providers/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TaskModule } from './modules/task/task.module';
+import { UserModule } from './modules/user/user.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [AuthModule, TaskModule, DatabaseModule],
+  imports: [AuthModule, TaskModule, DatabaseModule, UserModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService]
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 
 @Controller("api/auth")
 export class AuthController{
     constructor(private  readonly authSvc: AuthService){}
 
-    @Get()
+    @Post("login")
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation( {summary: 'Extrae el Id del usuario desde el token y busca la informacion'})
     public login(): string{
         return this.authSvc.logIn();
+    }
+
+    @Get("me")
+    public async getProfile(){
+
     }
 
 }
