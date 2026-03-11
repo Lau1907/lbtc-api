7901213f0242cddbc81cd31f370b9a6334c6b05b
Lau1907 <130590443+Lau1907@users.noreply.github.com>
2026-03-11 17:46:29 -0600

    fix: Implementacion de JWT y logica para el inicio de sesion

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UnauthorizedException } from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 
 @Controller("api/auth")
 export class AuthController{
+    utilSvc: any;
     constructor(private  readonly authSvc: AuthService){}
 
     @Post("login")
     @HttpCode(HttpStatus.OK)
     @ApiOperation( {summary: 'Extrae el Id del usuario desde el token y busca la informacion'})
-    public login(): string{
-        return this.authSvc.logIn();
+    public async login(@Body() auth): Promise<any> {
+        const { username, password } = auth;
+
+        const user = await this.authSvc.getUserByUsername(username);
+
+        if(!user)
+            throw new UnauthorizedException('El usuario y/o contrseña son incorrectas');
+        if(await this.utilSvc.checkPassword(password, user.password!)){
+            const { password,...payload} = user;
+
+            const jwt = await this.utilSvc.generateJWT(payload);
+
+
+            return { access_token: jwt, refresh_token: ''};
+        }else{
+            throw new UnauthorizedException('El usuario y/o contrseña son incorrectas')
+        }
     }
 
     @Get("me")
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class AuthService {
+
+    constructor(private readonly prisma:PrismaService){ }
+
+    public async getUserByUsername(): Promise<User | null>{
+        return await this.prisma.user.findFirst({
+            where: { username }
+        });
+    }
     public logIn (){
         return "Sesion exitosa";
     }
